91a56c620c44105aa6834c3866e9b401b5dc3169
Farah Amri <farah.amri@student.ehb.be>
2021-11-11 16:43:27 +0100

    fix: changed get route

--- __tests__/start.test.js
+++ __tests__/start.test.js
@@ -1,7 +1,8 @@
 const app = require("../server.js");
 const request = require('supertest');
 
-it('tests if connection to endpoint is succesful', async() => {
-    const response = await request(app).get('/');
+it('tests if connection to endpoint is successful', async() => {
+    const response = await request(app).get('/api/plants');
     expect(response.statusCode).toEqual(200);
+    expect(response.body.status).toBe("success");
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ('express');
-const app = express(); 
+const app = express();
 
-app.get('/', async (req,res) => {
+app.get('/api/plants', async (req,res) => {
     res.json({"status": "success"});
 });
 
