f91dec8c19e601fa29da80f244e8f6824bab3918
Farah Amri <farah.amri@student.ehb.be>
2021-11-11 17:06:02 +0100

    feat: added delete and get rout based on id

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require ('express');
 const app = express();
+const cors = require('cors');
 
+//MIDDLEWARE
+app.use(cors());
+
+//ROUTES
 app.get('/api/plants', async (req,res) => {
     res.json({"status": "success"});
+});
+
+app.get('/api/plants/:plantId', async(req, res) => {
+    res.send("here you will get 1 plant based on id");
+});
+
+app.delete('/api/plants/:plantId', async(req, res) => {
+    res.send("deleted 1 plant based on id");
 });
 
 module.exports = app; 
