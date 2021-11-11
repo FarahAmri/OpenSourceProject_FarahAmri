287ad7be6f0f4adde0f3edf62ba5048f3f09472d
Farah Amri <farah.amri@student.ehb.be>
2021-11-11 23:47:56 +0100

    docs: added conventions

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,5 +1,10 @@
 const helpers = {
 
+     /**
+    * returns all columns and rows from table
+    * @param {db} string name of your database
+    * @returns database 
+    */
     getAllData(db){
         return db
         .select("*")
--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const pg = require('knex')({
     connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://postgres:admin@localhost:5432/plantsDB'
 });
 
+/**
+    * sends the rootfile back
+    * @returns rootfile 
+    */
 app.get('/', async(req,res) => {
     res.send("hello");
     // pg.select('*').table("planten").then((rows) => {        
@@ -22,22 +26,42 @@ app.get('/', async(req,res) => {
     // });
 });
 
+
+/**
+    * sends a list of all the plants in the database
+    * @returns json
+    */
 //ROUTES
 app.get('/api/plants', async (req,res) => {
     res.json({"status": "success"});
     //const db = req.app.get('db');
 });
 
+/**
+    * select 1 plant based on id
+    * @argument {id} int 
+    * @returns 1 plant
+    */
 app.get('/api/plants/:plantId', async(req, res) => {
     res.send("here you will get 1 plant based on id");
 });
 
+/**
+    * deletes 1 plant based on id
+    * @argument {id} int 
+    * @returns deleted row
+    */
 app.delete('/api/plants/:plantId', async(req, res) => {
     res.send("deleted 1 plant based on id");
 });
 
+/**
+    * updates 1 plant based on id
+    * @argument {id} int 
+    * @returns changed row
+    */
 app.put('/api/plants/:plantId', async(req, res) => {
     res.send("changed 1 plant");
 });
 
-module.exports = app; 
+module.exports = app;
