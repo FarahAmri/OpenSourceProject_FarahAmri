f16636d02e2de58f30132e6d2b5c556cf1e27cbc
Farah Amri <farah.amri@student.ehb.be>
2021-12-22 20:35:55 +0100

    fix: added the right open source documents

--- api/src/__tests__/start.test.js
+++ api/src/__tests__/start.test.js
@@ -2,6 +2,8 @@ const app = require("../server.js");
 const request = require('supertest');
 const { response } = require("../server.js");
 
+//binnen testmap extra map maken om testen te scheiden van elkaar
+
 it('tests if connection to endpoint is successful', async() => {
     const response = await request(app).get('/');
     expect(response.statusCode).toEqual(200);
